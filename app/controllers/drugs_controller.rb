65b91511ee45be69617a693aa862dcd4e52ec999
Danil Pismenny <danil@orionet.ru>
2014-12-13 13:19:11 +0400

    DrugsQuery

--- app/controllers/drugs_controller.rb
+++ app/controllers/drugs_controller.rb
@@ -1,14 +1,12 @@
 class DrugsController < ApplicationController
   def index
-    if drugs_search_form.q.present?
-      result = DrugsIndex.
-        query( prefix: { name: drugs_search_form.query } ).
-        highlight(fields: { name: {"term_vector" => "with_positions_offsets"} }).
-        page(params[:page])
-    else
-      result = DrugsIndex::Drug.filter{ match_all }.page(params[:page])
-    end
-    render locals: { drugs_search_results: result, drugs_search_form: drugs_search_form }
+    render locals: { drugs_search_results: query.result, drugs_search_form: drugs_search_form }
+  end
+
+  private
+
+  def query
+    DrugsQuery.new form: drugs_search_form, page: params[:page]
   end
 
 end
--- app/controllers/pharmacies_controller.rb
+++ app/controllers/pharmacies_controller.rb
@@ -1,11 +1,22 @@
 class PharmaciesController < ApplicationController
+  helper_method :pharmacy
+
   def show
-    @pharmacy = Pharmacy.find params[:id]
-    pharmacy = @pharmacy
-    @drugs_search_results = DrugsIndex::Drug.filter{ pharmacy_id == pharmacy.id }.page(params[:page])
+    render locals: { drugs_search_results: query.result, drugs_search_form: drugs_search_form }
   end
 
   def index
     @pharmacies = Pharmacy.ordered.page params[:page]
   end
+
+  private
+
+  def pharmacy
+    @pharmacy ||= Pharmacy.find params[:id]
+  end
+
+  def query
+    DrugsQuery.new form: drugs_search_form, page: params[:page], pharmacy: pharmacy
+  end
+
 end
