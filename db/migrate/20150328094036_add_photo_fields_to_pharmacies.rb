9e53f5e2158224d4ccc1dcdb8f7213d7eeed5559
Danil Pismenny <danil@orionet.ru>
2015-03-30 22:37:01 +0400

    fix migrations

--- db/migrate/20150328094036_add_photo_fields_to_pharmacies.rb
+++ db/migrate/20150328094036_add_photo_fields_to_pharmacies.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-class CityCanBeNullInVendors < ActiveRecord::Migration
+class AddPhotoFieldsToPharmacies < ActiveRecord::Migration
 
   def up
     # These are extensions that must be enabled in order to support this database
