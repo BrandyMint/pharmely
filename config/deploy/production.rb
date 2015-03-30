47140113af64b0cc1a99d7ce7d8eb5f95669741d
Danil Pismenny <danil@orionet.ru>
2015-03-30 10:20:59 +0400

    Change repo in cap config

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,6 @@
 set :application, 'apteka.ws'
 set :stage, :production
-set :repo_url, 'https://github.com/BrandyMint/pharmely.git'
+set :repo_url, 'git@github.com:BrandyMint/pharmely.git'
 #ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
 set :deploy_to, ->{"/home/pharmely/#{fetch(:application)}"}
 server 'icfdev.ru', user: 'pharmely', port: 246, roles: %w{web app sidekiq db}
