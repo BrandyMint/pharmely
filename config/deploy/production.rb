b2705275340405c481c0d1b750371e038c303c2d
Denis Khabarov <admin@saymon21-root.pro>
2015-08-12 15:22:01 +0300

    update production deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,8 +2,8 @@ set :application, 'apteka.ws'
 set :stage, :production
 set :repo_url, 'git@github.com:BrandyMint/pharmely.git'
 #ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
-set :deploy_to, ->{"/home/pharmely/#{fetch(:application)}"}
-server 'icfdev.ru', user: 'pharmely', port: 246, roles: %w{web app sidekiq db}
+set :deploy_to, ->{"/home/wwwapteka/#{fetch(:application)}"}
+server '128.199.44.98', user: 'wwwapteka', port: 22, roles: %w{web app sidekiq db}
 set :rails_env, :production
 set :branch, ENV['BRANCH'] || 'master'
 fetch(:default_env).merge!(rails_env: :production)
